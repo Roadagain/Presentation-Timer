c225949af387d2d23dbc11594867b00ac8b229b0
Roadagain <ringoh72@gmail.com>
2015-12-15 23:21:55 +0900

    タイマー部分の文字列生成を別関数に分離した

--- timer.js
+++ timer.js
@@ -1,10 +1,17 @@
+function formatTime(num){
+    var minute = '0' + String(Math.floor(num / 60));
+    var second = '0' + String(num % 60);
+
+    return minute.substr(-2) + ':' + second.substr(-2);
+}
+
 function count(current, maximum){
     if (current > maximum){
         return;
     }
 
     var timer = document.getElementById('timer');
-    timer.innerText = Math.floor(current / 60) + ':' + current % 60;
+    timer.innerText = formatTime(current);
 
     window.setTimeout(count, 1000, current + 1, maximum);
 }
